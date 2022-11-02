82409ce6aaa389d20b72cb34de0e6d5aaa6171c1
gervasiuswisely <97431665+gervasiuswisely@users.noreply.github.com>
2022-11-02 21:51:28 +0700

    draft 02-11-2022

--- index.php
+++ index.php
@@ -5,11 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Joki Infor</title>
     <link rel="icon" href="./brunch-removebg-preview 1.png" type="image-xicon">
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Pacifico&family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
+  <nav class="navbar navbar-expand-lg bg-light ">
+  <div class="container">
     <a class="navbar-brand" href="#"><img src="./brunch-removebg-preview 1.png" alt="" style="width:150px"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -31,10 +35,52 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Apa yang kami sediakan?</a>
         </li>
+        
       </ul>
+      <button class="hubungikami">Hubungi Kami</button>
     </div>
   </div>
+
 </nav>
+<div class="container-fluid">
+    <div class="container">
+        <div class="row hero mx-auto">
+            <div class="col-6 merasa ">
+           <h1>Merasa Kesulitan dan Gak Punya Waktu?<br>
+                Jokiin aja!</h1> 
+            <p>Jokiin semua tugas website, landing page, maupun design ke kita. <br> Dijamin hasil puas, kamu pun senang.</p>
+            <button class="mulai">Mulai yuk!</button>
+            <button class="lihatproduk">Lihat produk</button>
+            </div>
+            <div class="col-6">
+                <div class="frame">
+                    <img src="./4881547-removebg-preview.png" alt="">
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row d-flex text-center">
+        <div class="col-4">
+        <div class="lingkaran1">
+        <p>oi</p>
+        </div>
+        </div>
+        <div class="col-4 align-self-end">
+       <h2>Mengapa Memilih Kami?</h2> 
+       <p>Santai, fleksibel, dan gak kaku. Kapanpun waktu yang kamu butuhin, kami siap membantu!</p>
+        </div>
+        <div class="col-4">
+    <div class="lingkaran2">
+        <p>oi</p>
+    </div>
+    </div>
+    </div>
+ 
+  
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
